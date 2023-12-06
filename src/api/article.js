facc42680e501e70607f4a62187f691b9e261942
Mikasaak <1474691736@qq.com>
2023-12-06 16:19:13 +0800

    文章分类基本完成

--- src/api/article.js
+++ src/api/article.js
@@ -3,3 +3,17 @@ import request from '@/utils/request'
 export function articleGetChannelListService() {
   return request.get('/my/cate/list')
 }
+
+export function articleAddChannelService({ cate_name, cate_alias }) {
+  return request.post('/my/cate/add', { cate_name, cate_alias })
+}
+
+export function articleUpdateChannelService({ id, cate_name, cate_alias }) {
+  return request.put('/my/cate/info', { id, cate_name, cate_alias })
+}
+
+export function articleDeleteChannelService(id) {
+  return request.delete('/my/cate/del', {
+    params: { id }
+  })
+}
--- src/views/article/ArticleChannel.vue
+++ src/views/article/ArticleChannel.vue
@@ -1,8 +1,12 @@
 <script setup>
 import PageContainer from '@/components/PageContainer.vue'
 import { ref } from 'vue'
-import { articleGetChannelListService } from '@/api/article'
+import {
+  articleDeleteChannelService,
+  articleGetChannelListService
+} from '@/api/article'
 import { Delete, Edit } from '@element-plus/icons-vue'
+import ChannelEdit from '@/views/article/components/ChannelEdit.vue'
 
 const loading = ref(true)
 const channelList = ref([])
@@ -13,12 +17,40 @@ const getChannelList = async () => {
   console.log(res)
 }
 getChannelList()
+const dialog = ref()
+const onEditChannel = (row) => {
+  dialog.value.open(row)
+  console.log(row)
+}
+
+const onDelChannel = async (row) => {
+  await ElMessageBox.confirm('此操作将永久删除该分类, 是否继续?', '提示', {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消',
+    type: 'warning'
+  })
+  await articleDeleteChannelService(row.id)
+  ElMessage({
+    message: '删除成功',
+    type: 'success'
+  })
+  getChannelList()
+}
+
+const onAddChannel = () => {
+  dialog.value.open()
+}
+const onSuccess = () => {
+  getChannelList()
+}
 </script>
 
 <template>
+  <channel-edit ref="dialog" @success="onSuccess"></channel-edit>
+
   <page-container title="文章分类">
     <template #extra>
-      <el-button type="primary"> 添加分类 </el-button>
+      <el-button type="primary" @click="onAddChannel"> 添加分类 </el-button>
     </template>
     <el-table :data="channelList" style="width: 100%" v-loading="loading">
       <el-table-column label="序号" width="100" type="index"> </el-table-column>
